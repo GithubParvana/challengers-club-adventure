67be87507b5177ba44fe0d405b78477e9fbd114a
Parvana BMU <46266509+GithubParvana@users.noreply.github.com>
2021-01-20 19:21:30 +0400

    Add files via upload

---
+++ SimpleArraySum.py
@@ -0,0 +1,23 @@
+import os
+# import sys
+
+
+#
+# Complete the simpleArraySum function below.
+#
+def simple_array_sum(ar):
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simple_array_sum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
